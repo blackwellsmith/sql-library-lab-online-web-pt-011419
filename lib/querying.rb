39032b8ebccf954ccfe6e22d5784d560e8753f76
blackwell <blackwellsmithiv@gmail.com>
2019-03-26 13:36:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.names, subgenres.names FROM subgenres LEFT OUTER JOIN series "
+  "SELECT authors.names, subgenres.names FROM subgenres LEFT OUTER JOIN series  ON subgenres.d = series.id"
 end
 
 def select_series_title_with_most_human_characters
