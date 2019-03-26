ee2c352e394afd249f967f12c6e5cf9bcab660d8
blackwell <blackwellsmithiv@gmail.com>
2019-03-26 14:12:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,5 +28,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
-  FROM character_books "
+  FROM character_books
+  LEFT OUTER JOIN ON "
 end
