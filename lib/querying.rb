340e47851012d426549d714f88063840fded4b1b
blackwell <blackwellsmithiv@gmail.com>
2019-03-26 14:05:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   WHERE characters.species = "human"
   GROUP BY series.title
-  ORDER by"
+  ORDER BY COUNT("
 end
 
 def select_character_names_and_number_of_books_they_are_in
