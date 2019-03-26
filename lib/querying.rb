8a1aae8fd72aa66f52c5d0b577f18d61f1c4200e
blackwell <blackwellsmithiv@gmail.com>
2019-03-26 14:05:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,8 @@ def select_series_title_with_most_human_characters
   LEFT OUTER JOIN characters 
   ON series.id = characters.series_id
   WHERE characters.species = "human"
-  GROUP BY "
+  GROUP BY series.title
+  ORDER by"
 end
 
 def select_character_names_and_number_of_books_they_are_in
